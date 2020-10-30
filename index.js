750db417eba103eea0d7b8c1528325bd5373b6e8
richard savala <richardsavala@my.cwi.edu>
2020-10-30 01:07:23 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const greatWhiteShark = {
+  myKingdom: 'Animalia',
+  myPyhlum: 'Chordata',
+  myClass: 'Chondrichtyes',
+  myOrder: 'Lamniformes',
+  myFamily: 'Charcharodon',
+  mySpecies: 'Charcharias',
+}
+
+console.log(greatWhiteShark.myKingdom); 
+
+greatWhiteShark.conservationStatus = 'vulnerable';
+
+greatWhiteShark.mySpecies = 'charcharias';
+
+console.log(greatWhiteShark);
